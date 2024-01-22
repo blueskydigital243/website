fd429a1d19b95243d3d20d78f6c1b12b4ff5b5e6
rkota(Rosaire Kota) <rosairekota@anicns.gouv.cd>
2024-01-22 12:19:25 +0100

    refactor: add configs

---
+++ app/providers/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export const ThemeProviders=({ children }: { children: React.ReactNode })=> {
+  return (
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+      {children}
+    </ThemeProvider>
+  );
+}
